9caf0bf6dab6e127084c73a2bfd6a86c6a185765
Stas <fedorkostas@gmail.com>
2020-12-29 12:55:03 +0100

    lesson 15

--- js/script.js
+++ js/script.js
@@ -1,37 +1,41 @@
 
-//- 14
+//- 15
 
-let num = 50;
+const numberOfFilms = +prompt('How many films do you watch?', '');
 
-// while (num <= 55) {
-//   console.log(num);
-//   num++;
-// }
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
 
-
-// do {
-//   console.log(num);
-//   num++;
-// }
-// while (num < 55);
-
-
-// for (let i = 1; i < 8; i++) {
-//   console.log(num);
-//   num++;
-// }
-
-
-for (let i = 1; i < 10; i++) {
-  if (i === 6) {
-    // break;
-    continue;
+for (let i = 0; i < 2; i++) {
+  const a = prompt('What was the last movie you watched?', ''),
+        b = prompt('Set the rating of this movie', '');
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
   }
-  console.log(i);
 }
 
+if (personalMovieDB.count < 10) {
+  console.log('you have seen few films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('you are a classic spectator');
+} else if (personalMovieDB.count >= 30) {
+  console.log('you are a movie fan');
+} else {
+  console.log('error');
+}
 
-//- end 14
+console.log(personalMovieDB);
+
+//- end 15
 
 
 
