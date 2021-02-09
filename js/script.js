3938fc4d20c153c0b0b83764dbe26bd3b92e4edc
Stas <fedorkostas@gmail.com>
2021-02-10 00:20:16 +0100

    17 lesson

--- js/script.js
+++ js/script.js
@@ -1,44 +1,36 @@
 
-//- 16
+//- 17
 
-let num = 20;
+const str = "test";
+console.log(str.length);
+const arr = [1, 2, 4];
+console.log(arr.length);
 
-function showFirstMessage(text) {
-  console.log(text);
-  let num = 10;
-  console.log(num);
-}
-
-showFirstMessage("Hello Stas");
-
-console.log(num);
-
-function calc(a, b) {
-  return (a + b);
-}
-
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
-
-function ret() {
-  let num = 50;
-  return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function() {
-  console.log("Hello");
-};
-
-logger();
-
-const calc2 = (a, b) => {return a + b};
+console.log(str[2]);
 
 
-//- end 16
+console.log(str.toLocaleUpperCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf('fruit'));
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));
+console.log(logg.slice(6, 9));
+
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num =12.2;
+console.log(Math.round(num));
+
+const test2 = "15.2px";
+console.log(parseInt(test2));
+console.log(parseFloat(test2));
+
+
+//- end 17
 
 
 
