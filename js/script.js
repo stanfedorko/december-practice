b02a83ee19f92c0f58bd13ee7c83fec260ed369c
Stas <fedorkostas@gmail.com>
2021-02-15 18:34:30 +0100

    22 lesson

--- js/script.js
+++ js/script.js
@@ -1,39 +1,97 @@
-//- 21
+//- 22
 
-const arr = [2, 13, 26, 8, 10];
-arr.sort(compareNum);
-// arr[99] = 0;
-// console.log(arr.length);
-console.log(arr);
+let a = 5,
+    b = a;
 
-function compareNum(a, b) {
-  return a- b;
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+  a: 5,
+  b: 1
+};
+
+// const copy = obj; // Link
+
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+
+function copy (mainObj) {
+  let objCopy = {};
+
+  let key;
+  for (key in mainObj) {
+    objCopy[key] = mainObj[key];
+  }
+
+  return objCopy;
 }
 
+const numbers = {
+  a: 2,
+  b: 5,
+  c: {
+    x:7,
+    y: 4
+  }
+};
 
-// arr.forEach(function(item, i, arr) {
-//   console.log(`${i}: ${item} внктри массива ${arr}`);
-// });
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+  d: 17,
+  e: 20
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'adddkgkk';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+console.log(internet);
 
 
-// arr.pop(); // remove
-// arr.push(10); // add 
-// console.log(arr);
+function log(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
 
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
+const num = [2, 5, 7];
 
-// for (let value of arr) {
-//   console.log(value);
-// }
-
-// console.log(arr);
+log(...num);
 
 
-// const str = prompt("", "");
-// const products = str.split(", ");
-// products.sort();
-// console.log(products.join('; '));
+const array = ['a', 'b'];
 
-//- end 21
+const newArray2 = [...array];
+
+const q = {
+  one: 1,
+  two: 2
+};
+
+const newObj = {...q};
+
+
+
+//- end 22
