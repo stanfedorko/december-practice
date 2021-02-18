285e7f344298155eb36c214005a6bce822b71dc3
Stas <fedorkostas@gmail.com>
2021-02-18 15:33:18 +0100

    23 lesson

--- js/script.js
+++ js/script.js
@@ -1,97 +1,30 @@
-//- 22
+//- 23
 
-let a = 5,
-    b = a;
 
-b = b + 5;
+let str = "some";
+let strObj = new String(str);
 
-console.log(b);
-console.log(a);
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
-const obj = {
-  a: 5,
-  b: 1
-};
+console.dir([1,2,3]);
 
-// const copy = obj; // Link
-
-// copy.a = 10;
-// console.log(copy);
-// console.log(obj);
-
-function copy (mainObj) {
-  let objCopy = {};
-
-  let key;
-  for (key in mainObj) {
-    objCopy[key] = mainObj[key];
-  }
-
-  return objCopy;
-}
-
-const numbers = {
-  a: 2,
-  b: 5,
-  c: {
-    x:7,
-    y: 4
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function() {
+    console.log("Hello");
   }
 };
 
-const newNumbers = copy(numbers);
+const jonh = Object.create(soldier);
 
-newNumbers.a = 10;
-console.log(newNumbers);
-console.log(numbers);
+// jonh.__proto__ = soldier;
 
-const add = {
-  d: 17,
-  e: 20
-};
+Object.setPrototypeOf(jonh, soldier);
 
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'adddkgkk';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-console.log(internet);
+console.log(jonh.armor);
+jonh.sayHello();
 
 
-function log(a, b, c) {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-
-const array = ['a', 'b'];
-
-const newArray2 = [...array];
-
-const q = {
-  one: 1,
-  two: 2
-};
-
-const newObj = {...q};
-
-
-
-//- end 22
+//- end 23
